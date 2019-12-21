f9a3dcc5811a6157e6cd8f2a87b4265910d40ece
mohammedalam <mohammedalam@10.211.55.3>
2019-12-20 21:09:32 -0500

    alam

--- Selenium_automation/src/main/java/com/usa/smarttech/MyFirstClass.java
+++ Selenium_automation/src/main/java/com/usa/smarttech/MyFirstClass.java
@@ -3,13 +3,7 @@ package com.usa.smarttech;
 public class MyFirstClass {
 
 	public static void main(String[] args) {
-driver = webdriver.Firefox()
 
-# Resize the window to the screen width/height
-driver.set_window_size(300, 500)
-
-# Move the window to position x/y
-driver.set_window_position(200, 200)
 
 	}
 
