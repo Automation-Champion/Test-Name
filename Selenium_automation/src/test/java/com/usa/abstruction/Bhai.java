0cd1eecae64ffaf7fbb5ead79bf23158adbe3cd1
mohammedalam <mohammedalam@10.211.55.3>
2019-12-20 20:59:45 -0500

    Alam

--- Selenium_automation/src/test/java/com/usa/abstruction/Bhai.java
+++ Selenium_automation/src/test/java/com/usa/abstruction/Bhai.java
@@ -9,9 +9,10 @@ public abstract class Bhai {
 	  
 	  
 	  public abstract  void sapace();
-	  
+	     
 	  
 	  public void employeesMethod() {
+		  System.out.println("this is new test"); 
 			System.out.println("this is new test");  
 			//System.out.println("this is github test"); 
 			//System.out.println("this is checking account");
